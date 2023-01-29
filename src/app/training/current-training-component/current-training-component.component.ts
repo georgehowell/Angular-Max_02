3cb8fd6c658693a3206c92700c3f5efe59460c81
georgehowell <geordiehowell@gmail.com>
2023-01-29 22:45:38 +1100

    2nd: Section 4 almost complete

--- src/app/training/current-training-component/current-training-component.component.ts
+++ src/app/training/current-training-component/current-training-component.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { MatDialog } from '@angular/material'
 
 @Component({
   selector: 'app-current-training-component',
   templateUrl: './current-training-component.component.html',
   styleUrls: ['./current-training-component.component.scss']
 })
-export class CurrentTrainingComponent {
+export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+  // timer: number;
 
+  // constructor(private dialog: MatDialog, private trainingService: TrainingService ) {}
+
+  ngOnInit() {
+    // this.startOrResumeTimer();
+  }
+
+  onStop() {
+    // clearInterval(this.timer);
+  }
 }
---
+++ src/app/training/current-training-component/mat-dialog/mat-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-dialog',
+  templateUrl: './mat-dialog.component.html',
+  styleUrls: ['./mat-dialog.component.scss']
+})
+export class MatDialogComponent {
+
+}
